c2a2a3514c35ced995c49bc3bff5e289f84259a1
Haruo <haruo-mtok@outlook.com>
2025-10-19 17:13:40 +0900

    index.html以外のHTMLもプロジェクトに含める

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,12 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     base: '/EnglishDB', // これを追加！
+    build: {
+        rollupOptions: {
+            input: {
+                main: "index.html",
+                flashcard: "flashcard.html",
+            },
+        },
+    },
 })
